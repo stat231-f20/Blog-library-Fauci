0cd7c76826fd5c6a80c813496f9b5c1bf7da7440
anayak23 <anayak23@amherst.edu>
2020-11-10 12:28:50 -0500

    Divided state text by "\r\n"

--- new_data_scraping.R
+++ new_data_scraping.R
@@ -1,6 +1,7 @@
 library(tidyverse) 
 library(rvest)
 library(robotstxt)
+library(stringr)
 
 #check if scraping is allowed
 paths_allowed("https://www.cdc.gov/healthyyouth/policy/state_law_summary_reports.html")
@@ -40,4 +41,21 @@ for(i in 1:50){
     }
   )
 }
+
+split_cdc_text <- data.frame(matrix(ncol = 54, nrow = 0))
+
+for(i in 1:50){
+  
+  split_cdc_text[,i] <- tryCatch(
+    # This is what I want to do...
+    {
+      print(str_split(cdc_scraped[i,3], "\r\n"))
+    }
+    # ... but if an error occurs, set to Missing and keep going 
+    , error = function(error_message) {
+      return("Missing")
+    }
+  )
+}
+
 
