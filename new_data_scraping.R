ff21670dbd2b6492cd621b01b16ab998279568f2
csarosi <csaros6@gmail.com>
2020-11-14 12:18:55 -0500

    Modified lines 46 and 53 to get the code running consistently

--- new_data_scraping.R
+++ new_data_scraping.R
@@ -9,19 +9,19 @@ paths_allowed("https://www.cdc.gov/healthyyouth/policy/state_law_summary_reports
 #initialize empty data frame to collect data from each state's cdc site
 cdc_state_data <- data.frame(matrix(ncol = 3, nrow = 0))
 cdc_col_names <- c("State", "URL", "CDC Text")
-colnames(cdc_state_data) <- cdc_names
+colnames(cdc_state_data) <- cdc_col_names
 
-data <- read_csv("project_data.csv")
+project_data <- read_csv("project_data.csv")
 state_names <- as.vector(data[,1])
 
 cdc_state_data <- rbind(cdc_state_data, data.frame(State = state_names))
 
-cdc_final <- cdc_state_data %>%
+cdc_scraped <- cdc_state_data %>%
   mutate(URL = paste0("https://www.cdc.gov/healthyyouth/policy/txt/summary_report_factsheets/",
                       State,
                       ".txt"))
 
-cdc_scraped <- cdc_final
+#cdc_scraped <- cdc_final
 
 for(i in 1:50){
   
@@ -43,14 +43,14 @@ for(i in 1:50){
 }
 
 
-split_cdc_text <- data.frame(matrix(ncol = 54, nrow = 0))
+split_cdc_text <- data.frame(matrix(ncol = 54, nrow = 54))
 
 for(i in 1:50){
   
   split_cdc_text[,i] <- tryCatch(
     # This is what I want to do...
     {
-      str_split(cdc_scraped[i,3], "\r\n")
+      lapply(str_split(cdc_scraped[i,3], "\r\n"), `length<-`, 54)
     }
     # ... but if an error occurs, set to Missing and keep going 
     , error = function(error_message) {
