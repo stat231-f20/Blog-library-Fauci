f002c868bb4d3c113a1999f40ca76b13def83f4e
laurenpelosi <lpelosi22@amherst.edu>
2020-11-18 22:34:56 -0500

    table works

--- lauren_blog3.R
+++ lauren_blog3.R
@@ -1,4 +1,4 @@
-#x-axis is student behavior, y-axis is health outcome
+#x-axis is State, y-axis is school and teacher practice, colored by mandate
 
 library(tidyverse)
 library(shinythemes)
@@ -24,6 +24,8 @@ data <- read.csv("data/new-project-data.csv") %>%
     TRUE ~ as.numeric(std_edu)
   ))
 
+definitions <- read.csv("data/practices_definitions.csv")
+
 gen_ed_txt <- read_csv("data/education-gen-text.csv") %>%
   rename(State = State_txt)
 
@@ -40,71 +42,104 @@ content_ed_txt[39,1] <- "Utah"
 life_ed_txt <- read_csv("data/education-life-text.csv") %>%
   rename(State = State_txt)
 
-x_choices <- as.list(names(data)[36:38])
-x_data_names <- c(names(data)[36:38])
-x_choice_names <- c("% of students that have had 4+ sexual partners",
-                    "% of students intoxicated in last sexual intercourse",
-                    "% of students that used condom in last sexual intercourse")
+# define vectors for choice values and labels
+# for selectInput, needs to be named list
+x_choices <- as.list(names(data)[2:22])
+x_data_names <- c(names(data)[2:22])
+x_choice_names <- c("Sex Ed Mandated",
+                    "HIV Ed Mandated",
+                    "Medically Accurate",
+                    "Age Appropriate",
+                    "Culturally Appropriate/Unbiased",
+                    "Cannot Promote Religion",
+                    "Parents Notified",
+                    "Parental Consent",
+                    "Opt-Out",
+                    "Sex Ed Contraception",
+                    "Sex Ed Abstinence",
+                    "Sex Ed Marriage",
+                    "Sex Ed Orientation",
+                    "Sex Ed Negative Outcomes",
+                    "HIV Ed Condoms",
+                    "HIV Ed Abstinence",
+                    "Healthy Relationships",
+                    "Sexual decision-making and self-discipline",
+                    "Refusal skills and personal boundaries",
+                    "Consent",
+                    "Dating and sexual violence prevention")
 names(x_choices) <- x_choice_names
+print(x_choices)
 
-y_choices <- as.list(names(data)[23:30])
-y_data_names <- c(names(data)[23:30])
-y_choice_names <- c("Chlamydia Per 100K",
-                    "Syphilis Per 100K",
-                    "Gonorrhea Per 100K",
-                    "Married Household %",
-                    "% HS Grad or Higher",
-                    "STD Index",
-                    "Births Per 1K Girls",
-                    "HIV Per 100K")
+y_choices <- as.list(names(data)[31:35])
+y_data_names <- c(names(data)[31:35])
+y_choice_names <- c("Provided Educators with Strategies",
+                    "Taught how STDs are Transmitted",
+                    "Increased Student Knowledge on Sexuality",
+                    "Increased Student Knowledge on HIV Prevention",
+                    "Increased Student Knowledge on STD Prevention")
 names(y_choices) <- y_choice_names
 
-ui <- navbarPage("Student Behavior and Health Outcomes in the United States:",
+ui <- navbarPage("Sexual Education Mandates and Health Outcomes in the United States:",
                  
-                 tabPanel("Student Behavior vs. Health Outcome",
+                 tabPanel("School Practices and Teacher Behaviors by State",
                           sidebarPanel(
                             selectInput(inputId = "x"
-                                        , label = "Choose a student behavior:"
+                                        , label = "Choose an education mandate of interest:"
                                         , choices = x_choices
-                                        , selected = "% of students that have had 4+ sexual partners"),
+                                        , selected = "Sex_Mandated"),
                             selectInput(inputId = "y"
-                                        , label = "Choose a health outcome:"
+                                        , label = "Choose a practice or behavior:"
                                         , choices = y_choices
-                                        , selected = "Chlamydia Per 100K")
+                                        , selected = "Provided Educators with Strategies")
                           ),
                           
                           mainPanel(
                             tabsetPanel(type = "tabs"
-                                        , tabPanel("Scatterplot", 
+                                        , tabPanel("Histogram",
+                                                   verbatimTextOutput(outputId = "check"),
+                                                   dataTableOutput(outputId = "table"),
                                                    plotlyOutput(outputId = "bar")
-                                                   ,tags$h6("* Yes, but only if county pregnancy rate is at least 19.5
-or higher per 1K for girls aged 15—17")   
-                                                   ,tags$h6("** Yes, Negative and mandated HIV education teaches that,
-among other behaviors, “homosexual activity” is considered
-to be “responsible for contact with the AIDS virus")     
-                                        )))))
+                                                   #                                                    ,tags$h6("* Yes, but only if county pregnancy rate is at least 19.5
+                                                   # or higher per 1K for girls aged 15—17")   
+                                                   #                                                    ,tags$h6("** Yes, Negative and mandated HIV education teaches that,
+                                                   # among other behaviors, “homosexual activity” is considered
+                                                   # to be “responsible for contact with the AIDS virus")     
+                                        )
+                                        
+                            ))))
 
 server <- function(input,output){
   
   use_data <- reactive({
     data <- data %>%
-      select(input$x, input$y)  %>%
+      select(State, input$x, input$y)  %>%
       left_join(gen_ed_txt, by = "State") %>%
       left_join(content_ed_txt, by = "State") %>%
       left_join(life_ed_txt, by = "State")
   })
   
+  definitions_reactive <- reactive({
+    definitions <- filter(definitions, String == input$y) %>%
+      select(Variable, Definition)
+  })
+  
+  output$check <- renderPrint({input$y})
   
   #Hist Tab  -------
-  output$scatter <- renderPlot({
-    ggplot(use_data(), aes(x = get(input$x), reorder(get(input$y)), get(input$x), y = get(input$y), x = get(input$x))) +
-      geom_point(stat = "identity") +
-      labs(y = y_choice_names[y_choices == input$y],
-           title = paste(paste(names(y_choices)[y_choices == input$y]))) +
-      theme(axis.title.x=element_blank(), axis.text.x=element_blank(),
-            axis.ticks.x=element_blank()) #+
-    #scale_fill_distiller(palette = "Set2", name = paste(paste(names(x_choices)[x_choices == input$x])))
+  output$bar <- renderPlotly({
+     ggplot(use_data(), aes(x = reorder(State, get(input$y)), y = get(input$y), fill = factor(get(paste(input$x))))) +
+       geom_bar(stat = "identity") +
+       labs(y = y_choice_names[y_choices == input$y],
+            title = paste(paste(names(y_choices)[y_choices == input$y]), "by State")) +
+       theme(axis.title.x=element_blank(), axis.text.x=element_blank(),
+             axis.ticks.x=element_blank()) #+
+  #scale_fill_distiller(palette = "Set2", name = paste(paste(names(x_choices)[x_choices == input$x])))
   })
+  
+  output$table <- renderDataTable({
+    definitions_reactive()
+  })
+  
 }
 
 
